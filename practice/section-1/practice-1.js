098f97fcbd97c2a9c2ad77a24db11995e674c998
liyijun123 <52227521+liyijun123@users.noreply.github.com>
2019-08-21 10:23:43 +0800

    Update practice-1.js

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -1,5 +1,13 @@
 'use strict';
-
+    let result;
+  
+  
+    
 function find(collection, element) {
-  return '实现练习要求，并改写该行代码。';
+ for(let i=0; i<collection.length; i++){
+   if(collection[i]===element){
+     return result=collection[i];
+   }
+ }
+  return '查无此数';
 }
